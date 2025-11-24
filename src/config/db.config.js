35b4f83c7e5f47e4b8b29fb26de9bd3ba5b9cc68
Sahil90 <sahil.laskar@techmapperz.com>
2025-11-24 15:59:22 +0530

    Fix Mongoose strictQuery deprecation warning

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// Suppress Mongoose deprecation warning
+mongoose.set('strictQuery', false);
+
 const connect = async()=>{
     return mongoose.connect(process.env.MONGO_URL)
 }
