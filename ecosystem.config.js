93e7c9e6ae10e5a9f5631fdc7ed3461230b16e11
Sahil90 <sahil.laskar@techmapperz.com>
2026-06-05 12:01:45 +0530

    chore: remove hardcoded PM2 log file paths from ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,9 +16,6 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 8080
       },
-      error_file: '/var/log/pm2/techmapperz-backend-error.log',
-      out_file: '/var/log/pm2/techmapperz-backend-out.log',
-      log_file: '/var/log/pm2/techmapperz-backend-combined.log',
       time: true,
       restart_delay: 1000,
       kill_timeout: 5000
